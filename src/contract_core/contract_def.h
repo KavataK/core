b679bc73e949f1eab702bb25dc4ceb0dad854ba3
KavataK <victor.tsiushkevich@gmail.com>
2025-11-21 10:59:50 +0300

    QBOND_CYCLIC_START_EPOCH:187

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -282,7 +282,7 @@ constexpr struct ContractDescription
     {"NOST", 172, 10000, sizeof(NOST)}, // proposal in epoch 170, IPO in 171, construction and first use in 172
     {"QDRAW", 179, 10000, sizeof(QDRAW)}, // proposal in epoch 177, IPO in 178, construction and first use in 179
     {"RL", 182, 10000, sizeof(RL)}, // proposal in epoch 180, IPO in 181, construction and first use in 182
-    {"QBOND", 187, 10000, sizeof(QBOND)}, // proposal in epoch 180, IPO in 181, construction and first use in 182
+    {"QBOND", 182, 10000, sizeof(QBOND)}, // proposal in epoch 180, IPO in 181, construction and first use in 182
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(TESTEXA)},
